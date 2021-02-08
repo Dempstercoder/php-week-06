33e8cbe35107738d774e78a4a6adfb1638a7d443
Dempsey <dempsey.rhian.neal@gmail.com>
2021-02-08 17:11:28 +0000

    completed challenge 05

---
+++ 02-arrays-functions/05-divide.php
@@ -0,0 +1,15 @@
+<?php
+
+// Create a function, divide, that takes two numbers as arguments. The function should return the first number divided by the second number.
+
+require __DIR__ . "/vendor/autoload.php";
+
+function divide ($number1, $number2) { // … your function here
+    return $number1 / $number2;
+};
+
+
+dump(divide(4, 2)); // 2
+dump(divide(20, 2)); // 10
+dump(divide(5, 2)); // 2.5
+dump(divide(2.5, 0.5)); // 5
