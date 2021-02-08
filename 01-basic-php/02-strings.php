4028725e1509b9a6798f260cc882f595432f6650
Dempsey <dempsey.rhian.neal@gmail.com>
2021-02-08 13:05:45 +0000

    added //i Using concatenation

--- 01-basic-php/02-strings.php
+++ 01-basic-php/02-strings.php
@@ -2,6 +2,7 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
+//i Using concatenation
 $string1 = "I";
 $string2 = "like";
 $string3 = "to";
@@ -10,3 +11,4 @@ $string5 = "boogie";
 
 $joined = $string1 . " " . $string2 . " " . $string3 . " " . $string4 . " " . $string5;/* your code here */
 dump($joined); // "I like to boogie boogie"
+
